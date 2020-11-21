9b8a26022103abbea1b9602df84bcb32a232a25b
pixdrift <pixdrift@users.noreply.github.com>
2020-11-22 00:33:13 +1100

    Change podman exec method to podman cp
    Avoid piping script to podman exec due to podman issue with version shipped with EL8.

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-currentDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+currentDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 # shellcheck source=base.sh
 source "${currentDir}"/base.sh
 
-if ! podman exec -i "$CONTAINER_ID" /bin/bash < "$1"
+# podman cp to work around podman exec bug with pipes in EL8 shipped version
+if ! (podman cp --pause=false "${1}" "${CONTAINER_ID}:/tmp" && \
+      podman exec -i "${CONTAINER_ID}" /bin/bash /tmp/$(basename "${1}"))
 then
     # Exit using the variable, to make the build as failure in GitLab CI.
     exit "$BUILD_FAILURE_EXIT_CODE"
