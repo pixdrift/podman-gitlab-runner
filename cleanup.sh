68d53b412b4872e2277fc0c45113ce50e67150f6
pixdrift <pixdrift@users.noreply.github.com>
2020-11-22 00:38:46 +1100

    Remove volumes from prune
    Minor changes to prune and NF syntax changes

--- cleanup.sh
+++ cleanup.sh
@@ -2,19 +2,19 @@
 
 TMPDIR=$(pwd)
 
-currentDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+currentDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 # shellcheck source=base.sh
 source "${currentDir}"/base.sh
 
-echo "Deleting container $CONTAINER_ID"
+echo "Deleting container ${CONTAINER_ID}"
 
-podman kill "$CONTAINER_ID"
-podman rm "$CONTAINER_ID"
+podman kill "${CONTAINER_ID}"
+podman rm "${CONTAINER_ID}"
 
-# Try to remove all old containers, images, networks, and volumes
-podman system prune --force --volumes
+# Try to remove all old containers, images, networks
+podman system prune --force
 
 # Delete leftover files in /tmp
-rm -r "$TMPDIR"
+rm -r "${TMPDIR}"
 
 exit 0
